2a02b0b484df9bf971b6e1c20aec355d17cad092
Joshua Haupt <joshua@hauptj.com>
2018-05-22 21:18:24 -0500

    added Vagrantfile, cleaned up roles

---
+++ config.rb
@@ -0,0 +1,27 @@
+##### Docker Swarm Dev Master
+
+# Hyper-V box
+$swarm_dev_hv_box = "hauptj/CentOS75"
+$swarm_dev_hv_box_ver = "22.5.2018"
+
+# VB box
+
+# General configuration
+$swarm_dev_vmname = "swarm"
+$swarm_dev_vcpus = "4"
+$swarm_dev_vmem = "4096"
+$swarm_dev_mac = "ECB8B9AFE1F1"
+
+
+##### Local Docker host to run Jenkins and AWX
+# Hyper-V box
+$docker_local_hv_box = "hauptj/CentOS75"
+$docker_local_hv_box_ver = "22.5.2018"
+
+# VB box
+
+# General configuration
+$docker_local_vmname = "docker"
+$docker_local_vcpus = "4"
+$docker_local_vmem  = "4096"
+$docker_local_mac = "ECB8B9AFE1FA"
